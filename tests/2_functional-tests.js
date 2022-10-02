d68fd85cce7fc2601d1ecec5363a34df11373ebd
nickcoimbra <nicolascoimbra2@gmail.com>
2022-10-01 21:54:32 -0300

    add functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -57,7 +57,7 @@ suite('Functional Tests', function () {
         surname: 'da Verrazzano'
       })
       .end(function (err, res) {
-      assert.equal(res.status, 200)
+          assert.equal(res.status, 200)
           assert.equal(res.type, 'application/json')
           assert.equal(res.body.name, 'Giovanni')
           assert.equal(res.body.surname, 'da Verrazzano')
