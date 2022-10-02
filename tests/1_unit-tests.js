a384179c535ee93d4d024edc60ac5b0f898485d5
nickcoimbra <nicolascoimbra2@gmail.com>
2022-10-01 21:17:28 -0300

    add at least suite 15

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -108,16 +108,16 @@ suite('Unit Tests', function () {
   suite('Arrays', function () {
     // #11
     test('#isArray, #isNotArray', function () {
-      assert.fail(
+      assert.isNotArray(
         'isThisAnArray?'.split(''),
         'String.prototype.split() returns an array'
       )
-      assert.fail([1, 2, 3].indexOf(2), 'indexOf returns a number')
+      assert.isArray([1, 2, 3].indexOf(2), 'indexOf returns a number')
     })
     // #12
     test('Array #include, #notInclude', function () {
-      assert.fail(winterMonths, 'jul', "It's summer in july...")
-      assert.fail(backendLanguages, 'javascript', 'JS is a backend language')
+      assert.notInclude(winterMonths, 'jul', "It's summer in july...")
+      assert.include(backendLanguages, 'javascript', 'JS is a backend language')
     })
   })
 
@@ -129,23 +129,23 @@ suite('Unit Tests', function () {
   suite('Strings', function () {
     // #13
     test('#isString, #isNotString', function () {
-      assert.fail(Math.sin(Math.PI / 4), 'A float is not a string')
-      assert.fail(
+      assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string')
+      assert.isNotString(
         process.env.PATH,
         'An env variable is a string (or undefined)'
       )
-      assert.fail(JSON.stringify({ type: 'object' }), 'JSON is a string')
+      assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string')
     })
     // #14
     test('String #include, #notInclude', function () {
-      assert.fail('Arrow', 'row', "'Arrow' contains 'row'")
-      assert.fail('dart', 'queue', "But 'dart' doesn't contain 'queue'")
+      assert.include('Arrow', 'row', "'Arrow' contains 'row'")
+      assert.notInclude('dart', 'queue', "But 'dart' doesn't contain 'queue'")
     })
     // #15
     test('#match, #notMatch', function () {
       const regex = /^#\sname\:\s[\w\s]+,\sage\:\s\d+\s?$/
-      assert.fail(formatPeople('John Doe', 35), regex)
-      assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex)
+      assert.match(formatPeople('John Doe', 35), regex)
+      assert.notMatch(formatPeople('Paul Smith III', 'twenty-four'), regex)
     })
   })
 
