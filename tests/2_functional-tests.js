0365a5e70bb15c7ae44716ca6e67570f4d0009f7
nickcoimbra <nicolascoimbra2@gmail.com>
2022-10-01 21:37:04 -0300

    add functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,36 +1,36 @@
-const chai = require('chai');
-const assert = chai.assert;
+const chai = require('chai')
+const assert = chai.assert
 
-const server = require('../server');
+const server = require('../server')
 
-const chaiHttp = require('chai-http');
-chai.use(chaiHttp);
+const chaiHttp = require('chai-http')
+chai.use(chaiHttp)
 
 suite('Functional Tests', function () {
-  this.timeout(5000);
+  this.timeout(5000)
   suite('Integration tests with chai-http', function () {
     // #1
     test('Test GET /hello with no name', function (done) {
       chai
         .request(server)
-        .get('/hello')
+        .get('/hello?name=Nick')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
-          done();
-        });
-    });
+          assert.equal(res.status, 200)
+          assert.equal(res.text, 'hello Nick')
+          done()
+        })
+    })
     // #2
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Nick')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
-          done();
-        });
-    });
+          assert.equal(res.status, 200)
+          assert.equal(res.text, 'hello Nick')
+          done()
+        })
+    })
     // #3
     test('Send {surname: "Colombo"}', function (done) {
       chai
@@ -38,45 +38,43 @@ suite('Functional Tests', function () {
         .put('/travellers')
 
         .end(function (err, res) {
-          assert.fail();
+          assert.fail()
 
-          done();
-        });
-    });
+          done()
+        })
+    })
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+      assert.fail()
 
-      done();
-    });
-  });
-});
+      done()
+    })
+  })
+})
 
-const Browser = require('zombie');
+const Browser = require('zombie')
 
 suite('Functional Tests with Zombie.js', function () {
-  this.timeout(5000);
-
-
+  this.timeout(5000)
 
   suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
-      assert.isNotNull(browser.site);
-    });
-  });
+    test('should have a working "site" property', function () {
+      assert.isNotNull(browser.site)
+    })
+  })
 
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
+      assert.fail()
 
-      done();
-    });
+      done()
+    })
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
+      assert.fail()
 
-      done();
-    });
-  });
-});
+      done()
+    })
+  })
+})
